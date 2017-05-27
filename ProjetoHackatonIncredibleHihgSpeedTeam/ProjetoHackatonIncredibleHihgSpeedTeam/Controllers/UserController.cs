acbe1317b0e54613911c5fa7f347a035d33a16aa
Matheus Anido <matheus_apc2@hotmail.com>
2017-05-27 17:43:52 -0300

    .

---
+++ ProjetoHackatonIncredibleHihgSpeedTeam/ProjetoHackatonIncredibleHihgSpeedTeam/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoHackatonIncredibleHihgSpeedTeam.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListaEmpresas()
+        {
+            return View();
+        }
+
+        public ActionResult ListaEscolas()
+        {
+            return View();
+        }
+    }
+}
